d67a7ceb67ee2c82d3928d26e7d8e6f9f20ee7b6
gotfivedogs <gotfivedogs@gmail.com>
2018-09-08 19:02:07 -0400

    FizzBuzz

--- Section1/Fizzbuzz/FizzBuzz/Program.cs
+++ Section1/Fizzbuzz/FizzBuzz/Program.cs
@@ -9,10 +9,41 @@ namespace FizzBuzz
             Execute();
             Console.ReadLine();
         }
-
         static void Execute()
         {
-            //TODO:  Implement FizzBuzz
+            int num = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                num = i;
+                if (num % 15 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+
+                    Console.WriteLine("The number {0} is FizzBuzz!", num);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    continue;
+                }
+                if (num % 5 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("The number {0} is Buzz", num);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    continue;
+                }
+                if (num % 3 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("The number {0} is Fizz", num);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    continue;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    Console.WriteLine("The number {0} is BORING!", num);
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
         }
     }
 }
