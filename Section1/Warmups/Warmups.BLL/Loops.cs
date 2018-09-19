beea69b940a9a599a38f9a4fa5cef2248baf094e
gotfivedogs <gotfivedogs@gmail.com>
2018-09-18 20:57:56 -0400

    finished up through front 9 on the loops section

--- Section1/Warmups/Warmups.BLL/Loops.cs
+++ Section1/Warmups/Warmups.BLL/Loops.cs
@@ -40,84 +40,124 @@ namespace Warmups.BLL
 
         public bool DoubleX(string str)
         {
-            bool amIDoubleX = false;
-            int count = 0;
-            while (true)
+            for (int i = 0; i < str.Length - 1; i++)
             {
-
+                if (str[i].Equals('x'))
+                {
+                    if (str[i + 1].Equals('x'))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
             }
-            for (int i = 0; i < str.Length-1; i++)
-            {
-              
-
-            }
-            return amIDoubleX;
+            return false;
         }
 
         public string EveryOther(string str)
         {
-            throw new NotImplementedException();
+            string CompressedString = "";
+
+            for (int i = 0; i < str.Length; i += 2)
+            {
+                CompressedString += str[i];
+            }
+            return CompressedString;
         }
 
         public string StringSplosion(string str)
         {
-            throw new NotImplementedException();
+            string StringExplosion = "";
+            string NewString = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                StringExplosion = StringExplosion += str[i];
+                NewString = NewString + StringExplosion;
+            }
+            return NewString;
         }
 
         public int CountLast2(string str)
         {
-            throw new NotImplementedException();
+            int SubStringCount = 0;
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                if ((str.Substring(i, 2) == str.Substring(str.Length - 2, 2)))
+                {
+                    SubStringCount += 1;
+                }
+            }
+            return SubStringCount - 1;
         }
 
         public int Count9(int[] numbers)
         {
-            throw new NotImplementedException();
+            int NumberOfNines = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == 9)
+                {
+                    NumberOfNines += 1;
+                }
+            }
+            return NumberOfNines;
         }
 
         public bool ArrayFront9(int[] numbers)
         {
-            throw new NotImplementedException();
+            int NineCount = 0;
+            for (int i = 0; i < numbers.Length-1; i++)
+            {
+                if ((numbers[i] == 9))
+                {
+                    NineCount += 1;
+                }
+            }
+           return (NineCount >= 1) ;    
         }
 
-        public bool Array123(int[] numbers)
-        {
-            throw new NotImplementedException();
-        }
-
-        public int SubStringMatch(string a, string b)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string StringX(string str)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string AltPairs(string str)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string DoNotYak(string str)
-        {
-            throw new NotImplementedException();
-        }
-
-        public int Array667(int[] numbers)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool NoTriples(int[] numbers)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Pattern51(int[] numbers)
-        {
-            throw new NotImplementedException();
-        }
-
+    public bool Array123(int[] numbers)
+    {
+        throw new NotImplementedException();
     }
+
+    public int SubStringMatch(string a, string b)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string StringX(string str)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string AltPairs(string str)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string DoNotYak(string str)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Array667(int[] numbers)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool NoTriples(int[] numbers)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Pattern51(int[] numbers)
+    {
+        throw new NotImplementedException();
+    }
+
+}
 }
