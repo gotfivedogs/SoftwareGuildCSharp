0203679aff16303383bc1c5d7bf48dc92ec8dada
gotfivedogs <gotfivedogs@gmail.com>
2018-09-24 21:17:03 -0400

    warmups complete

--- Section1/Warmups/Warmups.BLL/Arrays.cs
+++ Section1/Warmups/Warmups.BLL/Arrays.cs
@@ -41,55 +41,131 @@ namespace Warmups.BLL
         }
         public int[] RotateLeft(int[] numbers)
         {
-            int Temp = numbers[1];
-            int[] Rotated  = { numbers[1],numbers[2],numbers[3]};
+            int Temp = numbers[0];
+            int[] Rotated = { numbers[1], numbers[2], Temp };
             return Rotated;
-            
+
         }
         public int[] Reverse(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers.Length == 1)
+            {
+                return numbers;
+            }
+            Array.Reverse(numbers);
+            return numbers;
         }
 
         public int[] HigherWins(int[] numbers)
         {
-            throw new NotImplementedException();
+            int Temp = 0;
+
+            if (numbers[0] > numbers[numbers.Length - 1])
+            {
+                Temp = numbers[0];
+            }
+            else
+            {
+                Temp = numbers[numbers.Length - 1];
+            }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = Temp;
+            }
+            return numbers;
         }
 
         public int[] GetMiddle(int[] a, int[] b)
         {
-            throw new NotImplementedException();
+            int[] Middle2 = { a[1], b[1] };
+            return Middle2;
         }
 
         public bool HasEven(int[] numbers)
         {
-            throw new NotImplementedException();
+            if ((numbers[0] % 2 == 0) || (numbers[1] % 2 == 0))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public int[] KeepLast(int[] numbers)
         {
-            throw new NotImplementedException();
+            int[] DoubleTheLength = new int[numbers.Length * 2];
+            DoubleTheLength[DoubleTheLength.Length - 1] = numbers[numbers.Length - 1];
+            return DoubleTheLength;
         }
 
         public bool Double23(int[] numbers)
         {
-            throw new NotImplementedException();
+            int Count2 = 0;
+            int Count3 = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == 2)
+                {
+                    Count2++;
+                }
+                else
+                if (numbers[i] == 3)
+                {
+                    Count3++;
+                }
+            }
+            return Count2 == 2 || Count3 == 2;
         }
 
         public int[] Fix23(int[] numbers)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == 2 && numbers[i + 1] == 3)
+                {
+                    numbers[i + 1] = 0;
+                }
+            }
+            return numbers;
         }
 
         public bool Unlucky1(int[] numbers)
         {
-            throw new NotImplementedException();
+            if ((numbers[0] == 1 && numbers[1] == 3)
+                || numbers[1] == 1 && numbers[2] == 3
+                || (numbers[numbers.Length - 2] == 1
+                && numbers[numbers.Length - 1] == 3))
+                return true;
+            else
+                return false;
         }
 
         public int[] Make2(int[] a, int[] b)
         {
-            throw new NotImplementedException();
-        }
+            int Temp1 = 0;
+            int Temp2 = 0;
 
+            if (a.Length >= 2)
+            {
+                Temp1 = a[0];
+                Temp2 = a[1];
+            }
+            else
+            if (a.Length == 1)
+            {
+                Temp1 = a[0];
+                Temp2 = b[0];
+            }
+            else
+        if (a.Length == 0)
+            {
+                Temp1 = b[0];
+                Temp2 = b[1];
+            }
+           int[] NewTwoArray = { Temp1, Temp2 };
+            return NewTwoArray;
+        }
     }
 }
